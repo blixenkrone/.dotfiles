3f2becc2fe34f338a76d2df933ea58004d1fa889
Simon Blixenkrone <sblixenkrone@gmail.com>
2022-03-17 11:33:03 +0100

    init sh file

---
+++ init.zsh
@@ -0,0 +1,5 @@
+## Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
