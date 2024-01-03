a43a979ca74c4da5793e035536779d766bf28f3a
Simon Blixenkrone <sblixenkrone@gmail.com>
2024-01-03 12:31:01 +0100

    amended init script

--- init.zsh
+++ init.zsh
@@ -1,5 +1,22 @@
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 40 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 150       # normal minimum is 2 (30 ms)
+
+echo "Added defaults"
+
 ## Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! type "brew" >/dev/null; then
+    echo "installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "brew already installed"
+fi
 
 ## Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [[ $ZSH == "" ]]; then
+    echo "installing omz"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "omz already installed"
+fi
