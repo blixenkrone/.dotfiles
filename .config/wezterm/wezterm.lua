75f40c15e86ff28939329d26ed8e007c6a29c50e
Simon Blixenkrone <sblixenkrone@gmail.com>
2024-07-22 22:59:41 +0200

    more cfg

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,6 +16,23 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 15
 
+-- config.use_dead_keys = false
+-- config.send_composed_key_when_left_alt_is_pressed = false
+-- config.send_composed_key_when_right_alt_is_pressed = true
+
+config.keys = {
+    -- Map commonly used Option key combinations to their Alt equivalents
+    -- You can add more key bindings as needed
+    -- {key="a", mods="ALT", action=wezterm.action.SendString("\x1ba")},
+    -- {key="b", mods="ALT", action=wezterm.action.SendString("\x1bb")},
+    -- {key="c", mods="ALT", action=wezterm.action.SendString("\x1bc")},
+    -- {key="9", mods="ALT", action=wezterm.action.SendString("\x1b]")},
+    {key="i", mods="ALT", action=wezterm.action.SendString("|")},
+    {key="8", mods="ALT", action=wezterm.action.SendString("[")},
+    {key="8", mods="ALT|SHIFT", action=wezterm.action.SendString("{")},
+    {key="9", mods="ALT|SHIFT", action=wezterm.action.SendString("}")},
+    {key="9", mods="ALT", action=wezterm.action.SendString("]")},
+}
 -- Return the configuration to wezterm
 return config
 
