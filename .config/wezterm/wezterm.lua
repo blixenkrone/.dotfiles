8b2618afc1cf2cfdc055c98abe29d7ccf14d7381
Simon Blixenkrone <sblixenkrone@gmail.com>
2024-07-22 12:45:06 +0200

    added wezterm cfg

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Apply config
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 14
+config.color_scheme = "Batman"
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 15
+
+-- Return the configuration to wezterm
+return config
+
