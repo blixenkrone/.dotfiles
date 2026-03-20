f92cf0439cc2841450a53edefe69474a07cac33f
Simon Blixenkrone <sblixenkrone@gmail.com>
2026-03-20 20:08:11 +0100

    macos defaults

---
+++ macos/set-defaults.sh
@@ -0,0 +1,142 @@
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
+
+set +e
+
+disable_agent() {
+	mv "$1" "$1_DISABLED" >/dev/null 2>&1 ||
+		sudo mv "$1" "$1_DISABLED" >/dev/null 2>&1
+}
+
+unload_agent() {
+	launchctl unload -w "$1" >/dev/null 2>&1
+}
+
+sudo -v
+
+echo ""
+echo "› System:"
+echo "  › Disable press-and-hold for keys in favor of key repeat"
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+echo "  › Show the ~/Library folder"
+chflags nohidden ~/Library
+
+echo "  › Show the /Volumes folder"
+sudo chflags nohidden /Volumes
+
+echo "  › Set a really fast key repeat"
+defaults write -g InitialKeyRepeat -int 4
+defaults write -g KeyRepeat -int 3
+
+echo "  › Require password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo "  › Always show scrollbars"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+echo "  › Disable Dashboard"
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+echo "  › Don't automatically rearrange Spaces based on most recent use"
+defaults write com.apple.dock mru-spaces -bool false
+
+
+echo "  › Disable smart quotes and smart dashes as they're annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+echo "  › Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo "  › Set up trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+echo "  › Avoid the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+echo "  › Set dark interface style"
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+echo "  › Set graphite appearance"
+defaults write NSGlobalDomain AppleAquaColorVariant -int 6
+
+echo "  › Set graphite highlight color"
+defaults write NSGlobalDomain AppleHighlightColor -string "0.847059 0.847059 0.862745"
+
+echo "  › Show battery percent"
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+
+echo ""
+echo "› Finder:"
+echo "  › Always open everything in Finder's list view"
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+echo "  › Set the Finder prefs for showing a few different volumes on the Desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+echo "  › Expand save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+echo "  › Set sidebar icon size to small"
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+echo "  › Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "  › Show path bar"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "  › Disable the warning before emptying the Trash"
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+echo "  › Save to disk by default, instead of iCloud"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo "  › Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo "  › Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo ""
+echo "› Photos:"
+echo "  › Disable it from starting everytime a device is plugged in"
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+echo ""
+echo "› Dock"
+echo "  › Setting the icon size of Dock items to 45 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 45
+
+echo "  › Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock expose-group-by-app -bool true
+
+echo "  › Remove the auto-hiding Dock delay"
+defaults write com.apple.dock autohide-delay -float 0
+
+echo "  › Remove the animation when hiding/showing the Dock"
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+echo "  › Show hidden apps in the Dock"
+defaults write com.apple.Dock showhidden -bool true
+
+echo "  › Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "  › Don't animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false
+
+echo ""
+echo "› Restart related apps"
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+	"Terminal" "Transmission" "Photos"; do
+	killall "$app" >/dev/null 2>&1
+done
+set -e
